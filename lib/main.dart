e162113973f5166159739f49edeccdfd7b00fd60
trungtin10 <tintran2k4@gmail.com>
2025-04-07 13:54:09 +0700

    Lưu tạm

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'test.dart'; // Import file test
-=======
+
+
+
 import 'package:total_english/screens/login_screen.dart';
 import 'package:total_english/screens/main_screen.dart';
 import 'package:total_english/screens/signup_screen.dart';
->>>>>>> 9af244ad87b4f309e75a998a9b16cf8cf2659d9a
+
 
 void main() {
   runApp(const MyApp());
@@ -18,13 +18,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-      home: TestScreen(), // Hiển thị màn hình test
-=======
-      home: LoginScreen(),
-      //home: MainScreen(),
+
+
+
+      //home: LoginScreen(),
+      home: MainScreen(),
       //home: SignupScreen(),
->>>>>>> 9af244ad87b4f309e75a998a9b16cf8cf2659d9a
+
     );
   }
 }
